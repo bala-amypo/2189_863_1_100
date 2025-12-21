71c497303f85f61ad359701e22e9da32516af346
bala-amypo <bala@amypo.in>
2025-12-21 09:07:50 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/DocumentTypeService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.model.DocumentType;
+import java.util.List;
+
+public interface DocumentTypeService {
+    
+    DocumentType createDocumentType(DocumentType type);
+    
+    List<DocumentType> getAllDocumentTypes();
+    
+    DocumentType getDocumentType(Long id);
+}
