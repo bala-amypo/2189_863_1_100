6fad1a7d36472258e6c9dc3ea3d2c95aaa8cad1e
bala-amypo <bala@amypo.in>
2025-12-27 06:47:41 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,3 +1,56 @@
+// package com.example.demo.config;
+
+// import com.example.demo.security.JwtAuthenticationFilter;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+// @Configuration
+// @EnableMethodSecurity
+// public class SecurityConfig {
+
+//     private final JwtAuthenticationFilter jwtFilter;
+    
+//     public SecurityConfig(JwtAuthenticationFilter jwtFilter) {
+//         this.jwtFilter = jwtFilter;
+//     }
+
+//     @Bean
+//     public PasswordEncoder passwordEncoder() {
+//         return new BCryptPasswordEncoder();
+//     }
+
+//     @Bean
+//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+//         http
+//             .csrf(csrf -> csrf.disable())
+//             .cors(cors -> cors.disable())
+//             .authorizeHttpRequests(auth -> auth
+//                 .requestMatchers("/auth/**").permitAll()
+//                 .requestMatchers(
+//                     "/swagger-ui/**",
+//                     "/v3/api-docs/**",
+//                     "/api/**"
+//                 ).permitAll()
+//                 .anyRequest().authenticated()
+//             )
+//             .addFilterBefore(
+//                 jwtFilter,
+//                 UsernamePasswordAuthenticationFilter.class
+//             );
+
+//         return http.build();
+//     }
+// }
+
+
+
 package com.example.demo.config;
 
 import com.example.demo.security.JwtAuthenticationFilter;
@@ -32,17 +85,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .cors(cors -> cors.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**").permitAll()
-                .requestMatchers(
-                    "/swagger-ui/**",
-                    "/v3/api-docs/**",
-                    "/api/**"
-                ).permitAll()
-                .anyRequest().authenticated()
-            )
-            .addFilterBefore(
-                jwtFilter,
-                UsernamePasswordAuthenticationFilter.class
+                .anyRequest().permitAll()
             );
 
         return http.build();
